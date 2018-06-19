c70cb1c1a561e1ce993280ce1fddc521e973f82e
yellowk4 <yellowk4@naver.com>
2018-06-19 23:49:39 +0900

    주소창

--- sscnt/js/common.js
+++ sscnt/js/common.js
@@ -1104,28 +1104,21 @@ $(function() {
 });
 
 
-// 모바일 브라우저 주소창 안보이게 하기
-(function() {
+function hideAddressBar()
+{
+  if(!window.location.hash)
+  {
+      if(document.height < window.outerHeight)
+      {
+          document.body.style.height = (window.outerHeight + 50) + 'px';
+      }
 
-	function hideAddressBar() {
-	  
-	  // 웹페이지의 높이가 화면높이보다 작을 때는 실행할 필요가 없으므로 종료
-	  if (document.height <= window.outerHeight) return;
-	  
-	  var scrollTimer = setInterval(function() {
-	   if ( ! pageYOffset) {  // 모바일 브라우저에서 주소창이 보이고 있을 때는 pageYOffset = 0 이므로 이때만 실행
-		scrollTo(0, 1);  // 웹페이지를 x축 0, y축 1의 위치로 끌어올림
-	   } else { // pageYOffset !=0 인 경우 반복 종료: scrollTo(0, 1) 이 실행되었거나 사용자가 스크롤을 움직인 경우
-		clearInterval(scrollTimer);
-	   }
-	  }, 100); // 100 밀리세컨드마다 반복 실행
-	  
-	};
-	
-	window.addEventListener('load', hideAddressBar, false);  // 페이지 로드 되었을 때 실행
-	window.addEventListener('orientationchange', hideAddressBar, false); // 화면이 가로/세로 전환되었을 때 실행
-	
-	})(); // 페이지 내 다른 자바스크립트 코드에 영향을 주거나 받지 않도록 self executed anonymous function 형태로 만듬
+      setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+  }
+}
+
+window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
+window.addEventListener("orientationchange", hideAddressBar );
 
 	
 	
