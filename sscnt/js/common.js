6ad3fca31bb5555f2b01614eae4bc3c98ff97506
yellowk4 <yellowk4@naver.com>
2018-06-19 23:25:23 +0900

    주소창 숨기기

--- sscnt/js/common.js
+++ sscnt/js/common.js
@@ -1104,7 +1104,19 @@ $(function() {
 });
 
 
+function hideAddressBar(){
 
+	if(document.documentElement.scrollHeight<window.outerHeight/window.devicePixelRatio) 
+
+		document.documentElement.style.height=(window.outerHeight/window.devicePixelRatio)+'px'; 
+
+	setTimeout(window.scrollTo(1,1),0); 
+
+} 
+
+window.addEventListener("load",function(){hideAddressBar();}); 
+
+window.addEventListener("orientationchange",hideAddressBar());
 
 	
 	
