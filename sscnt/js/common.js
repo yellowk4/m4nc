e91ca8c9ad782be089035b047d510491c60add83
yellowk4 <yellowk4@naver.com>
2018-06-19 23:57:46 +0900

    주소창 포기

--- sscnt/js/common.js
+++ sscnt/js/common.js
@@ -1104,21 +1104,21 @@ $(function() {
 });
 
 
-function hideAddressBar()
-{
-  if(!window.location.hash)
-  {
-      if(document.height < window.outerHeight)
-      {
-          document.body.style.height = (window.outerHeight + 50) + 'px';
-      }
+// function hideAddressBar()
+// {
+//   if(!window.location.hash)
+//   {
+//       if(document.height < window.outerHeight)
+//       {
+//           document.body.style.height = (window.outerHeight + 50) + 'px';
+//       }
 
-      setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
-  }
-}
+//       setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+//   }
+// }
 
-window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
-window.addEventListener("orientationchange", hideAddressBar );
+// window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
+// window.addEventListener("orientationchange", hideAddressBar );
 
 	
 	
